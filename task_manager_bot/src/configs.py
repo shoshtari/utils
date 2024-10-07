be722da8c5ece12371df95e37897f667ee5850c5
shoshtari <mr.malekey@gmail.com>
2024-10-07 13:42:22 +0330

    feat: initialized task manager bot

---
+++ task_manager_bot/src/configs.py
@@ -0,0 +1,23 @@
+import os
+
+
+def get_env(env: str, default = "", needed: bool = False):
+    """ 
+    if needed is True, default will be ignored 
+    """
+
+    PREFIX = "BOT_"
+    if env_var := os.environ.get(PREFIX + env):
+        env_type = type(default)
+        return env_type(env_var)
+    elif needed:
+        raise ValueError(f"required env var {env} has not been set")
+    return default
+
+
+BOT_TOKEN = get_env("TOKEN", needed = True)
+API_ID = get_env("API_ID", needed = True)
+API_HASH = get_env("API_HASH", needed = True)
+
+
+ADMIN_ID = get_env("ADMIN_ID",  0, needed = True)
